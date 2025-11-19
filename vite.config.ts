002f7601f3d97d9d6cffa97594d5cb795645a134
nogadas-vonryan <vonnogadas251@gmail.com>
2025-11-19 19:18:43 +0800

    feat: integrate PWA support with vite-plugin-pwa and update manifest configuration
    - Added vite-plugin-pwa to package.json for PWA capabilities.
    - Removed existing public/manifest.json file.
    - Configured PWA settings in vite.config.ts, including app name, description, theme color, and icons.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,36 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.svg'],
+      manifest: {
+        name: 'ShammilliQuiz',
+        short_name: 'ShammilliQuiz',
+        description: 'A minimal cloze-type quiz for offline studying.',
+        theme_color: '#4d92ba',
+        icons: [
+          {
+            src: 'img/icons/quiz-icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'img/icons/quiz-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      },
+    })
+  ],
 
   base: '/minimal-quiz/'
 })
